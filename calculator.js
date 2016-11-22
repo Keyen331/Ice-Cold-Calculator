bff4510555ad33d71d17ea7392945d5de8b97413
Keyen331 <banayos.kay@gmail.com>
2016-11-21 19:43:04 -1000

    uploading

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function calculator(operator, initialValue, secondaryValue){
+  if (operator = 'addition') {
+		console.log(6 + 2)
+	}
+  if(operator = 'subtraction'){
+    console.log(6 - 2)
+  }
+  if(operator = 'division'){
+    console.log(6 / 2)
+  }
+  if(operator = 'multiplication'){
+    console.log(6 * 2)
+  }
+}
+
+calculator(['addition', 'subtraction', 'division', 'multiplication'], 6, 2)
+
